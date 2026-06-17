821298e141330a49114a0c74a9deded7b2157a5f
WildanFadillah1512 <wildan.fadillah@nusaputra.ac.id>
2026-06-17 17:22:52 +0700

    berkas st

--- app/spk/page.tsx
+++ app/spk/page.tsx
@@ -59,7 +59,7 @@ const getTodayDateString = () => {
     return `${year}-${month}-${day}`;
 };
 
-const BACKDATE_ALLOWED_BRANCHES = ['LAMPUNG', 'LUWU', 'JEMBER', 'CILACAP', 'BANJARMASIN', 'BANDUNG', 'BANDUNG 1', 'BANDUNG 2', 'BANDUNG RAYA', 'BANDUNG_RAYA'];
+const BACKDATE_ALLOWED_BRANCHES = ['LAMPUNG', 'LUWU', 'JEMBER', 'CILACAP', 'BANJARMASIN', 'BANDUNG', 'BANDUNG 1', 'BANDUNG 2', 'BANDUNG RAYA', 'BANDUNG_RAYA', 'CILEUNGSI'];
 
 // Tipe untuk data form yang dimuat saat revisi
 type RevisiFormSnapshot = {
