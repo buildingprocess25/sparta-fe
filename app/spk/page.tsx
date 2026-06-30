491022f3f0d24a1ed9131abeb4e4511b10ffdd06
WildanFadillah1512 <wildan.fadillah@nusaputra.ac.id>
2026-06-30 15:56:35 +0700

    test

--- app/spk/page.tsx
+++ app/spk/page.tsx
@@ -17,9 +17,11 @@ const getCabangCode = (cabangName: string) => {
     const map: Record<string, string> = {
         "WHC IMAM BONJOL": "7AZ1", "LUWU": "2VZ1", "KARAWANG": "1JZ1", "REMBANG": "2AZ1",
         "BANJARMASIN": "1GZ1", "PARUNG": "1MZ1", "TEGAL": "2PZ1", "GORONTALO": "2SZ1",
-        "PONTIANAK": "1PZ1", "LOMBOK": "1SZ1", "KOTABUMI": "LZ01", "SERANG": "2GZ1",
-        "CIANJUR": "2JZ1", "BALARAJA": "TZ01", "SIDOARJO": "UZ01", "MEDAN": "WZ01",
+        "PONTIANAK": "1PZ1", "LOMBOK": "1SZ1", "SUMBAWA": "1SZ1", "KOTABUMI": "LZ01", "SERANG": "2GZ1",
+        "CIANJUR": "2JZ1", "BALARAJA": "TZ01", "SIDOARJO": "UZ01", "SIDOARJO BPN_SMD": "UZ01",
+        "MANOKWARI": "UZ01", "NTT": "UZ01", "SORONG": "UZ01", "MEDAN": "WZ01", "ACEH": "WZ01",
         "BOGOR": "XZ01", "JEMBER": "YZ01", "BALI": "QZ01", "PALEMBANG": "PZ01",
+        "BENGKULU": "PZ01", "BANGKA": "PZ01", "BELITUNG": "PZ01",
         "KLATEN": "OZ01", "MAKASSAR": "RZ01", "PLUMBON": "VZ01", "PEKANBARU": "1AZ1",
         "JAMBI": "1DZ1", "HEAD OFFICE": "Z001", "BANDUNG 1": "BZ01", "BANDUNG 2": "NZ01",
         "BEKASI": "CZ01", "CILACAP": "IZ01", "CILEUNGSI": "JZ01", "SEMARANG": "HZ01",
