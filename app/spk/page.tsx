9253fc3ae77d0e20c6905cb6139a1894c8791ace
WildanFadillah1512 <wildan.fadillah@nusaputra.ac.id>
2026-07-17 11:18:54 +0700

    Solve bug Sidoarjo BPN pada SPK, dan migrasi Data Luwu dari Non Ruko ke Ruko

--- app/spk/page.tsx
+++ app/spk/page.tsx
@@ -18,7 +18,7 @@ const getCabangCode = (cabangName: string) => {
         "WHC IMAM BONJOL": "7AZ1", "LUWU": "2VZ1", "KARAWANG": "1JZ1", "REMBANG": "2AZ1",
         "BANJARMASIN": "1GZ1", "PARUNG": "1MZ1", "TEGAL": "2PZ1", "GORONTALO": "2SZ1",
         "PONTIANAK": "1PZ1", "LOMBOK": "1SZ1", "SUMBAWA": "1SZ1", "KOTABUMI": "LZ01", "SERANG": "2GZ1",
-        "CIANJUR": "2JZ1", "BALARAJA": "TZ01", "SIDOARJO": "UZ01", "SIDOARJO BPN_SMD": "UZ01",
+        "CIANJUR": "2JZ1", "BALARAJA": "TZ01", "SIDOARJO": "UZ01", "SIDOARJO BPN SMD": "UZ01", "SIDOARJO BPN_SMD": "UZ01",
         "MANOKWARI": "UZ01", "NTT": "UZ01", "SORONG": "UZ01", "MEDAN": "WZ01", "ACEH": "WZ01",
         "BOGOR": "XZ01", "JEMBER": "YZ01", "BALI": "QZ01", "PALEMBANG": "PZ01",
         "BENGKULU": "PZ01", "BANGKA": "PZ01", "BELITUNG": "PZ01",
