0e378cba0a449bb3a62f2f1f20cd2109e8ab4ec8
WildanFadillah1512 <wildan.fadillah@nusaputra.ac.id>
2026-08-09 00:47:57 +0700

    Revert "fix: decouple dashboard v2 refresh loading"
    This reverts commit 2c8e4b62532d34bb19d7724c5f0a75b73379bc84.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1657,8 +1657,21 @@ export default function DashboardPage() {
                         selectedBranch={selectedCabang}
                         onBranchChange={setSelectedCabang}
                         isSuperAdmin={canSeeAllMonitoringBranches}
-                        onRefresh={loadDashboardV2Data}
-                        isRefreshing={dashboardV2Loading}
+                        onRefresh={async () => {
+                            await Promise.all([
+                                fetchDashboardData(
+                                    userInfo.cabang,
+                                    canSeeAllMonitoringBranches,
+                                    user?.email ?? '',
+                                    userInfo.namaPt,
+                                    isCompanyScopedUser,
+                                    true,
+                                    userInfo.roles
+                                ),
+                                loadDashboardV2Data(),
+                            ]);
+                        }}
+                        isRefreshing={isDataLoading || dashboardV2Loading}
                         searchQuery={searchQuery}
                         onSearchChange={setSearchQuery}
                         jobType={dashboardV2JobType}
